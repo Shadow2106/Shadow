386e5218d4857907ac262629730e3b6b636e504b
Nithin <nithin21.v@gmail.com>
2025-02-26 13:03:17 +0530

    Slide

---
+++ script.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+let timer = setInterval(() => plusSlides(1), 5000); 
+
+function plusSlides(n) {
+  clearInterval(timer);
+  showSlides(slideIndex += n);
+  timer = setInterval(() => plusSlides(1), 5000); 
+}
+
+function currentSlide(n) {
+  clearInterval(timer); 
+  showSlides(slideIndex = n);
+  timer = setInterval(() => plusSlides(1), 5000); 
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
